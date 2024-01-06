a29a6fd9f08d5ff8da18e847c343902d41828b00
Samuel Muli <samuelmuli95@gmail.com>
2024-01-06 22:44:59 +0300

    Modifications

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
-document.addEventListener("DOMContentLoaded", async function() {
-    try{
-        const instance = await PSPDFKit.load({
-            container: "#pdf-container",
-            document: "path/to/your/document.pdf",
-            licenseKey: "your-license-key"
-        })
+import "./assets/dist/pspdfkit.js"
+import { PSPDFKit } from "./assets/dist/pspdfkit.js";
 
-        const searchInput = document.getElementById("search-input")
-        searchInput.addEventListener("input", function() {
-            const searchText = searchInput.ariaValueMax.trim().toLowerCase()
-            if(searchText.length > 0) {
-                instance.search(searchText)
-            }
-        })
-    }catch(error){
-        console.log("Error initializing PSPDFKit:", error)
-    }
+// document.addEventListener("DOMContentLoaded", async function() {
+
+
+//     try{
+//         const instance = await PSPDFKit.load({
+//             container: "#pdf-container",
+//             document: "document.pdf",
+//         })
+
+//         const searchInput = document.getElementById("search-input")
+//         searchInput.addEventListener("input", function() {
+//             const searchText = searchInput.ariaValueMax.trim().toLowerCase()
+//             if(searchText.length > 0) {
+//                 instance.search(searchText)
+//             }
+//         })
+//     }catch(error){
+//         console.log("Error initializing PSPDFKit:", error)
+//     }
+// })
+
+const baseUrl = `${window.location.protocol}//${window.location.host}/assets/`;
+
+PSPDFKit.load({
+    baseUrl,
+    container: "#pdf-container",
+    document: "document.pdf"
+})
+.then(instance => {
+    console.log("PSPDFKit loaded", instance)
+})
+.catch(error => {
+    console.log(error.message)
 })
